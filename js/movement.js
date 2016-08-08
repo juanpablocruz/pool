cc73143342978dabbc038667da1d950c9218e074
Juan Pablo <pablo@cruzf.net>
2016-08-08 14:18:13 +0200

    initial jp branch

--- js/movement.js
+++ js/movement.js
@@ -38,48 +38,6 @@ Bola.prototype.movement = function() {
 
 
 
-	/*var a = 0.5 * escenario.drag * Math.pow(this.tiempo,2);
-	this.tiempo += 1/33;
-
-	if (this.speed.x > 0) {
-			a *= (-1);
-	} else if (this.speed.x < 0) {
-			a *= (1);
-	}
-	
-	if (this.speed.y > 0) {
-			a *= (-1);
-	} else if (this.speed.y < 0) {
-			a *= (1);
-	}
-
-	
-	var x = this.Position.x + this.speed.x +a;
-	var y = this.Position.y + this.speed.y +a;
-
-	if (x-this.radio <= 0) {
-		this.speed.x *= (-1);
-		this.Position.x = this.radio;
-	} else if (x+this.radio >= escenario.width) {
-		this.speed.x *= (-1);
-		this.Position.x = escenario.width-this.radio;		
-	} else {
-		this.Position.x = x;
-	}
-
-
-	if (y-this.radio < 0) {
-		this.speed.y *= (-1);
-		this.Position.y = this.radio;
-	} else if (y+this.radio > escenario.height) {
-		this.speed.y *= (-1);
-		this.Position.y = escenario.height-this.radio;		
-	} else {
-		this.Position.y = y;
-	}
-
-	*/
-
 	this.checkCollision();
 
 	//console.log(Math.floor(this.speed.x), Math.floor(this.speed.y));
