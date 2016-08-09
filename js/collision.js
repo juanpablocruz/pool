7a97bac16f8432092609acd8c4b18f1c4a24eb80
Dorian <arhacai@gmail.com>
2016-08-09 23:52:04 +0200

    Now balls share speed when they collide

--- js/collision.js
+++ js/collision.js
@@ -43,8 +43,11 @@ Bola.prototype.checkCollisionHole = function() {
 
 Bola.prototype.collide = function(colTarget) {
 	var speedModule = this.speed.Length();
-	colTarget.obj.speed = colTarget.colPoint.substract(this.Position).multEsc(speedModule/colTarget.colPoint.distance(this.Position));
-	this.speed = V(colTarget.obj.speed.y, colTarget.obj.speed.x);
-	this.direction = this.direction.multEsc(-1);
+	var speedModule2 = colTarget.obj.speed.Length();
+	var speed = colTarget.colPoint.substract(this.Position).multEsc(1/colTarget.colPoint.distance(this.Position));
+	var newSpeed = (speedModule + speedModule2)/2;
+	colTarget.obj.speed = speed.multEsc(newSpeed);
+	this.speed = V(speed.y, speed.x).multEsc(newSpeed);
+	//this.direction = this.direction.multEsc(-1);
 	colTarget.obj.direction = colTarget.obj.direction.multEsc(-1);
 }
